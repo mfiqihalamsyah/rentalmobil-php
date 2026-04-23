9dab34a5e9ca73b866a0f659361f9e01b480030f
mfiqihalamsyah <faqoy20@gmail.com>
2026-04-23 14:19:26 +0700

    Fix: https dan path

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -17,7 +17,7 @@ $koneksi = new PDO("mysql:host=localhost;dbname=rentalmobil_php", $user, $pass);
 $con = mysqli_connect("localhost", "php_user", "php_user", "rentalmobil_php");
 
 global $url;
-$url = "http://localhost/rentalmobil-php/";
+$url = "https://localhost/rentalmobil-php/";
 
 $sql_web = "SELECT * FROM infoweb WHERE id = 1";
 $row_web = $koneksi->prepare($sql_web);
